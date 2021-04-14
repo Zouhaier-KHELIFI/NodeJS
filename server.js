5427c42a50aea8d6efac6364550add7030ee0762
zouhaier khelifi <khlifizouhaier8@gmail.com>
2021-04-14 14:41:29 +0100

    Backend

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-var http = require('http')
-var hostname = '127.0.0.1'
-var port = 3000
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.end('hello world')
+var app = require('express')();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.get("/", function(req, res) {
+
+    res.sendFile(__dirname + '/index.html')
 })
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+
+io.on("connection", function(socket) {
+
+    console.log('a user is connected');
+    socket.on('disconnect', function() {
+            console.log('a user is disconnected');
+
+
+        }
+
+    )
+
+    socket.on("chat message", function(message) {
+        console.log("message reçu : " + message)
+        io.emit("chat message", message)
+    })
+})
+http.listen(3000, function() {
+
+
+    console.log('hello')
 })
